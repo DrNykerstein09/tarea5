42aac36bab18d42fc0e7539a78228dc220178b41
DrNykerstein09 <88530440+DrNykerstein09@users.noreply.github.com>
2022-06-04 17:05:34 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const alturaCm = 175;
+const alturaM = 1.75;
+const peso = 68.4;
+const alturaRe = Math.ceil(alturaM);
+const pesoRe= Math.floor(peso);
+const iguales = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
+
+
+
